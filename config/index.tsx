9de3a9eb34d58539aa9c44a6612e4c98e60f5edb
Sy Trung <lesytrung2204@gmail.com>
2026-07-14 15:38:33 +0700

    feat: integrate SWR for dynamic portfolio data fetching and document database schema

--- config/index.tsx
+++ config/index.tsx
@@ -2,19 +2,23 @@ import { useToast } from "@/components/ui/use-toast";
 import { FacebookFilled, GithubFilled, InstagramFilled } from "@ant-design/icons"
 import axios from "axios";
 import { BriefcaseBusiness, Contact, Dumbbell, FolderKanban, Home, User } from "lucide-react";
+import useSWR from "swr";
 
-interface Contact {
+interface ContactData {
     fullName: string,
     email: string,
 }
 
+const fetcher = (url: string) => axios.get(url).then(res => res.data);
+
 export const config = () => {
+    const { toast } = useToast();
+    const { data } = useSWR("https://api.helloyouu.site/api/portfolio/all", fetcher);
+
     const currentDate = new Date();
     const currentYear = currentDate.getFullYear();
     const startWork = 2023;
     const yearOfExperience = currentYear - startWork;
-    const { toast } = useToast()
-
 
     const section = () => {
         return [
@@ -51,38 +55,48 @@ export const config = () => {
         ]
     }
 
-
     const info = () => {
+        const defaultSocial = [
+            { name: "Facebook", link: "https://www.facebook.com/ntduc106", open: true, icon: <FacebookFilled /> },
+            { name: "Instagram", link: "https://www.instagram.com/ntduc106", open: true, icon: <InstagramFilled /> },
+            { name: "Github", link: "https://github.com/ntducne", open: true, icon: <GithubFilled /> },
+        ];
+
+        if (data?.profile) {
+            return {
+                name: data.profile.name || "Nguyen Duc",
+                major: data.profile.introduce?.major_line_2 || "Software Engineer",
+                img: data.profile.avatar_url || "/img.jpg",
+                email: data.profile.email || "nguyenduc10603@gmail.com",
+                address: data.profile.address || "Hanoi, Vietnam",
+                social: defaultSocial,
+            }
+        }
         return {
             name: "Nguyen Duc",
             major: "Software Engineer",
             img: "/img.jpg",
             email: "nguyenduc10603@gmail.com",
             address: "Hanoi, Vietnam",
-            social: [
-                {
-                    name: "Facebook",
-                    link: "https://www.facebook.com/ntduc106",
-                    open: true,
-                    icon: <FacebookFilled />
-                },
-                {
-                    name: "Instagram",
-                    link: "https://www.instagram.com/ntduc106",
-                    open: true,
-                    icon: <InstagramFilled />
-                },
-                {
-                    name: "Github",
-                    link: "https://github.com/ntducne",
-                    open: true,
-                    icon: <GithubFilled />
-                },
-            ],
+            social: defaultSocial,
         }
     }
 
     const introduce = () => {
+        if (data?.profile?.introduce) {
+            const intro = data.profile.introduce;
+            return {
+                textType: intro.text_type || data.profile.name || "Nguyen Duc",
+                major: {
+                    line_1: intro.major_line_1 || "Web Designer and Backend",
+                    line_2: intro.major_line_2 || "Developer"
+                },
+                text: intro.text || "I design and code beautifully simple things and i love what i do. Just simple like that!",
+                year_of_experience: currentYear - (intro.start_work_year || startWork),
+                project_completed: intro.projects_completed || 20,
+                num_of_clients: intro.num_of_clients || 2
+            }
+        }
         return {
             textType: "Nguyen Duc",
             major: {
@@ -97,10 +111,18 @@ export const config = () => {
     }
 
     const about = () => {
-        return "I am a backend developer. I am passionate about designing and developing mining complex web applications in languages ​​like Javascript and PHP, using technologies like Laravel Framework, NextJS and NuxtJS. I also have experience working with databases like MongoDB, MySQL. My knowledge includes RESTful API, Docker. I am always up to date with the latest trends and always want to contribute to the development of the organization in the field of technology"
+        if (data?.profile?.about_text) return data.profile.about_text;
+        return "I am a backend developer. I am passionate about designing and developing mining complex web applications in languages ​​like Javascript and PHP, using technologies like Laravel Framework, NextJS and NuxtJS. I also have experience working with databases like MongoDB, MySQL. My knowledge includes RESTful API, Docker. I am always up to date with the latest trends and always want to contribute to the development of the organization in the field of technology";
     }
 
     const resume = () => {
+        if (data?.resumes && data.resumes.length > 0) {
+            return data.resumes.sort((a: any, b: any) => a.order - b.order).map((item: any) => ({
+                time: item.time_period,
+                title: item.title,
+                description: item.description
+            }));
+        }
         return [
             {
                 time: "October 2021 - February 2024",
@@ -126,6 +148,13 @@ export const config = () => {
     }
 
     const skills = () => {
+        if (data?.skills && data.skills.length > 0) {
+            return data.skills.sort((a: any, b: any) => a.order - b.order).map((item: any) => ({
+                name: item.name,
+                icon: item.icon_url,
+                animation: item.animation || "fade-up"
+            }));
+        }
         return [
             {
                 name: "NextJS",
@@ -166,6 +195,16 @@ export const config = () => {
     }
 
     const projects = () => {
+        if (data?.projects && data.projects.length > 0) {
+            return data.projects.sort((a: any, b: any) => a.order - b.order).map((item: any) => ({
+                name: item.name,
+                description: item.description,
+                img: item.image_url,
+                technology: item.technology || [],
+                website: "",
+                git: "",
+            }));
+        }
         return [
             {
                 name: "Hotel Management Website",
@@ -216,7 +255,7 @@ export const config = () => {
     };
 
 
-    const submitContact = async (data: Contact) => {
+    const submitContact = async (data: ContactData) => {
         var baseApi = "https://api.helloyouu.site/api/contact";
         try {
             await axios.post(baseApi, data);
