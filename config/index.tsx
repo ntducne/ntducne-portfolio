58684f9c73efaaf4889234b9ace1c6d8ba6174fd
Sy Trung <lesytrung2204@gmail.com>
2026-07-14 11:29:47 +0700

    chore: update contact form API endpoint to custom domain

--- config/index.tsx
+++ config/index.tsx
@@ -208,7 +208,7 @@ export const config = () => {
 
     const submitContact = async (data: Contact) => {
         // var baseApi = "http://localhost:5000/sendMessage";
-        var baseApi = "https://portfolio-s3j4.onrender.com/sendMessage";
+        var baseApi = "https://api.helloyouu.site/contact";
         try {
             await axios.post(baseApi, data);
             toast({
